634f4aa03d77aa935657a7f286effad86c58d400
Berke Tangör <bertangor21@gmail.com>
2023-11-07 16:18:19 +0300

    berkeodev

---
+++ react7kasim/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div classNameName="App">
+        <div classNameName="header">
+          <div className="logom">
+            <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?w=740&t=st=1699360856~exp=1699361456~hmac=d6e078a88bb51d0c27093d4eebe08b6f55f831efb50f6cefe11fde5b62e54f8d"/>
+          </div>
+          <div className="buton">
+            <ul className="buton-liste">
+              <li className="libuton"><a>Hakkımızda</a></li>
+              <li className="libuton"><a>Bilgiler</a></li>
+              <li className="libuton"><a>Uyelik aç</a></li>
+              <li className="libuton"><a>Giris Yap</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className="body">
+            <div className="Kategory">
+              <h2>Başlık</h2>
+              <p>dafsvgbsfdbdgbsd advdsbsfdb ascdsvfbdesfb ascdvfbsbs ascdvsfbw asvdsbf csadvsbfd csadvsbfd
+                cadsvfbg ascvdsbf avds casvdbf csavdsbfg csavdbsf SFAVDBSF favdbs vadbsg AVDBSF AFDGBS FWAEGRW
+              </p>
+            </div>
+            <div className="urun">
+              
+            </div>
+        </div>
+    </div>
+  );
+}
+
+
+function GirisYap(){
+  
+}
+export default App;

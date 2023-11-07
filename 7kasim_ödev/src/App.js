1547e2209a5d6c7de27f97c843d49c1e304129a1
Berke Tangör <bertangor21@gmail.com>
2023-11-07 21:52:29 +0300

    7 kasim ödevi

---
+++ 7kasim_ödev/src/App.js
@@ -0,0 +1,96 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+        <div className="header navbar navbar-expand-lg bg-body-tertiary bg-info-subtle">
+            <div className="container-fluid">
+              <a class="navbar-brand" href="#">
+              <img width="100" height="100" src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?w=740&t=st=1699360856~exp=1699361456~hmac=d6e078a88bb51d0c27093d4eebe08b6f55f831efb50f6cefe11fde5b62e54f8d"/>
+              </a>
+                <div className="buton">
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item"><a href="#" className="nav-link btn">Hakkımızda</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link btn">Bilgiler</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link btn">Uyelik aç</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link btn">Giris Yap</a></li>
+                  </ul>
+                </div>
+            </div>
+        </div>
+         <div className="body container text-center bg-warning-subtle">  
+            <div className="Category row">
+              <div className="col-sm-4 border">
+                <h2>Araçlar</h2>
+                <ul  className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item"><a href="#" className="nav-link btn">Motorlar</a></li>
+                  <li className="nav-item"><a href="#" className="nav-link btn">Arabalar</a></li>
+                  <li className="nav-item"><a href="#" className="nav-link btn">Otobüsler</a></li>
+                  <li className="nav-item"><a href="#" className="nav-link btn">Minibüsler</a></li>
+                  <li className="nav-item"><a href="#" className="nav-link btn">Kamyonlar</a></li>
+                  <li className="nav-item"><a href="#" className="nav-link btn">Tırlar</a></li>
+                </ul>
+              </div>
+              <div className="col-sm-8">
+                <div className="row">
+                    <div className="col-sm-4 border border-4 rounded-3 mt-2">
+                      <h2>Ürün Adı</h2>
+                      <img width="150" height="100" src="https://img.freepik.com/free-photo/set-colored-balloons-flying-ground-cappadocia-turkey_181624-20938.jpg?w=1060&t=st=1699379645~exp=1699380245~hmac=efbc757d38034bcfaa16885ab30753db3af2b4712486c6c400fa5c920907666a"/>
+                      <p className="text-start"> Ürün Açıklaması advbfb dsfvbdg xfvsfb dgn  sbdgnfc dvsv</p>
+                      <p>Ürün Fiyatı(TL)</p>
+                      <a className="btn">120</a>
+                    </div>
+                    <div className="col-sm-4 border border-4  rounded-3 mt-2">
+                      <h2>Ürün Adı</h2>
+                      <img width="150" height="100" src="https://img.freepik.com/free-photo/set-colored-balloons-flying-ground-cappadocia-turkey_181624-20938.jpg?w=1060&t=st=1699379645~exp=1699380245~hmac=efbc757d38034bcfaa16885ab30753db3af2b4712486c6c400fa5c920907666a"/>
+                      <p className="text-start"> Ürün Açıklaması advbfb dsfvbdg xfvsfb dgn  sbdgnfc dvsv</p>
+                      <p>Ürün Fiyatı(TL)</p>
+                      <a className="btn">120</a> 
+                    </div>
+                    <div className="col-sm-4 border border-4 rounded-3 mt-2">
+                      <h2>Ürün Adı</h2>
+                      <img width="150" height="100" src="https://img.freepik.com/free-photo/set-colored-balloons-flying-ground-cappadocia-turkey_181624-20938.jpg?w=1060&t=st=1699379645~exp=1699380245~hmac=efbc757d38034bcfaa16885ab30753db3af2b4712486c6c400fa5c920907666a"/>
+                      <p className="text-start"> Ürün Açıklaması advbfb dsfvbdg xfvsfb dgn  sbdgnfc dvsv</p>
+                      <p>Ürün Fiyatı(TL)</p>
+                      <a className="btn">120</a>
+                    </div>
+                  
+                </div>
+                <div className="row">
+                    <div className="col-sm-4 border border-4 rounded-3 mt-2">
+                      <h2>Ürün Adı</h2>
+                      <img width="150" height="100" src="https://img.freepik.com/free-photo/set-colored-balloons-flying-ground-cappadocia-turkey_181624-20938.jpg?w=1060&t=st=1699379645~exp=1699380245~hmac=efbc757d38034bcfaa16885ab30753db3af2b4712486c6c400fa5c920907666a"/>
+                      <p className="text-start"> Ürün Açıklaması advbfb dsfvbdg xfvsfb dgn  sbdgnfc dvsv</p>
+                      <p>Ürün Fiyatı(TL)</p>
+                      <a className="btn">120</a>                   
+                    </div>
+                    <div className="col-sm-4 border border-4 rounded-3 mt-2">
+                      <h2>Ürün Adı</h2>
+                      <img width="150" height="100" src="https://img.freepik.com/free-photo/set-colored-balloons-flying-ground-cappadocia-turkey_181624-20938.jpg?w=1060&t=st=1699379645~exp=1699380245~hmac=efbc757d38034bcfaa16885ab30753db3af2b4712486c6c400fa5c920907666a"/>
+                      <p className="text-start"> Ürün Açıklaması advbfb dsfvbdg xfvsfb dgn  sbdgnfc dvsv</p>
+                      <p>Ürün Fiyatı(TL)</p>
+                      <a className="btn">120</a>
+                    </div>
+                    <div className="col-sm-4 border border-4 rounded-3 mt-2">
+                      <h2>Ürün Adı</h2>
+                      <img width="150" height="100" src="https://img.freepik.com/free-photo/set-colored-balloons-flying-ground-cappadocia-turkey_181624-20938.jpg?w=1060&t=st=1699379645~exp=1699380245~hmac=efbc757d38034bcfaa16885ab30753db3af2b4712486c6c400fa5c920907666a"/>
+                      <p className="text-start"> Ürün Açıklaması advbfb dsfvbdg xfvsfb dgn  sbdgnfc dvsv</p>
+                      <p>Ürün Fiyatı(TL)</p>
+                      <a className="btn">120</a> 
+                    </div>
+                  
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+
+function GirisYap(){
+  
+}
+export default App;
